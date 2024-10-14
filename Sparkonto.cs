60f7c259a3e05eec59a912c3a029e412c02157df
petterFagerlund <110305733+petterFagerlund@users.noreply.github.com>
2024-10-14 22:05:31 +0200

    Update Sparkonto.cs

--- Sparkonto.cs
+++ Sparkonto.cs
@@ -1,5 +1,7 @@
-﻿public class Sparkonto : BankAccount
+//Todo: class not used
+public class Sparkonto : BankAccount
 {
+    //Todo: Naming convention is wrong and in swedish. 
     public Sparkonto(string owenerName, double initialBalance) : base(owenerName, initialBalance)
     {
     }
