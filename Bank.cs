dd810ec6422d8cc86a97c788f07cabbae4b590bf
petterFagerlund <110305733+petterFagerlund@users.noreply.github.com>
2024-10-14 22:06:49 +0200

    Update Bank.cs

--- Bank.cs
+++ Bank.cs
@@ -1,8 +1,12 @@
-﻿using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.X509Certificates;
 using System.Security.Principal;
 
 public class Bank
 {
+    //Todo: go trough encapsulation. Which properties need to be public? Otherwise private or internal. 
+    //Todo: setters seem redundant. If they never change, should be init. 
+    //Todo: Don´t write in swedish
+    //Todo: BankAccount should not be initialized from field. Rather return. 
     public BankAccount Personkonto { get; private set; }
     public BankAccount Sparkonto { get; private set; }
     public BankAccount Investeringskonto { get; private set; }
@@ -16,15 +20,19 @@ public class Bank
 
     public BankAccount GetAccountByNumber(string accountnumber)
     { 
+        //Todo: brackets after if condition or convert to ? operator.  (clearer for next person who reads. 
+        //Todo: Account number types should probably be extracted to its own enum not as strings. 
         if (Personkonto.AccountNumber == accountnumber) return Personkonto;
         if (Sparkonto.AccountNumber == accountnumber) return Sparkonto;
         if (Investeringskonto.AccountNumber == accountnumber) return Investeringskonto;
 
+        //Todo: Method does not allow null as return. 
         return null;
     }
 
     public void Deposit(BankAccount account, double amount)
     {
+        //Todo: account is never null
         if (account != null)
         {
             account.Deposit(amount);
@@ -37,6 +45,7 @@ public class Bank
 
     public void Withdraw(BankAccount account, double amount)
     {
+        //Todo: account is never null
         if (account != null)
         {
             account.Withdraw(amount);
@@ -49,6 +58,7 @@ public class Bank
     }
     public void Transfer(BankAccount fromAccount, BankAccount toAccount, double amount)
     {
+        //Todo: account is never null. Put validation in separate class
         if (fromAccount == null || toAccount == null)
         {
             Console.WriteLine("Invalid account number.");
