c001fcbe0fd5360ec3707608fca951fb51e651dc
petterFagerlund <110305733+petterFagerlund@users.noreply.github.com>
2024-10-14 22:05:49 +0200

    Update Personkonto.cs

--- Personkonto.cs
+++ Personkonto.cs
@@ -1,5 +1,7 @@
-﻿public class Personkonto : BankAccount
+//Todo:class not used, namespace wrong, also in swedish
+public class Personkonto : BankAccount
 {
+    //Todo: Naming convention is wrong and in swedish. 
     public Personkonto(string owenerName, double initialBalance) : base(owenerName, initialBalance)
     { 
     }
